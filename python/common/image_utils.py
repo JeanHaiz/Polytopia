7a7323d161b406356823df523514e7604060ab4d
JeanHaiz <jean.haizmann@gmail.com>
2022-01-23 19:48:29 +0100

    map size bug

--- python/common/image_utils.py
+++ python/common/image_utils.py
@@ -138,6 +138,7 @@ def get_cloud_template():
 
 
 def get_background_template(map_size: str):
-    if map_size == "0":
+    print("map size", map_size)
+    if map_size is None or map_size == "0":
         map_size = "400"
     return __get_template("background_template_%s.png" % map_size)[:, :, 0:3]
