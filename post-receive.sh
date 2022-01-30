e4a7f90d2c9ac75af30b0e3c3732473c33210f6d
JeanHaiz <jean.haizmann@gmail.com>
2022-01-30 12:00:04 +0100

    update post-reveice routine

---
+++ post-receive.sh
@@ -0,0 +1,19 @@
+echo Start of post-receive
+
+git fetch
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse master@{upstream})
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+then
+	echo Not up to date with origin. Pulling & Restarting.
+	git pull
+	chmod +x /Users/jean/Documents/Coding/Polytopia/post-receive.sh
+	docker-compose down
+	docker-compose up python
+	crontab crontab.txt
+else
+	echo Current branch is up to date with origin/master.
+fi
+
+echo End of post-receive
