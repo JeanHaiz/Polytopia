97b1e2f45ef8a9e4286bac5e42d896320ea9ea7e
JeanHaiz <jean.haizmann@gmail.com>
2022-02-07 00:13:57 +0100

    post-receive echo

--- post-receive.sh
+++ post-receive.sh
@@ -1,17 +1,22 @@
+echo $(pwd)
 echo Start of post-receive
 
-git fetch
+echo $(git fetch)
+
 HEADHASH=$(git rev-parse HEAD)
 UPSTREAMHASH=$(git rev-parse master@{upstream})
 
+echo $HEADHASH $UPSTREAMHASH
+
 if [ "$HEADHASH" != "$UPSTREAMHASH" ]
 then
-	echo Not up to date with origin. Pulling & Restarting.
-	git pull
-	chmod +x /Users/jean/Documents/Coding/Polytopia/post-receive.sh
-	docker-compose down
-	docker-compose up --detach helper
-	crontab crontab.txt
+	echo Not up to date with origin. Pulling and Restarting.
+	echo $(git pull)
+	echo $(chmod +x /Users/jean/Documents/Coding/Polytopia/post-receive.sh)
+	echo $(docker-compose down)
+	echo $(docker-compose up --detach helper)
+	echo $(crontab crontab.txt)
+	echo End of Pulling
 else
 	echo Current branch is up to date with origin/master.
 fi
