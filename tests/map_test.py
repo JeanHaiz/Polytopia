0145cd48fc095e56cc42e8969b2950bc5bbd7da3
JeanHaiz <jean.haizmann@gmail.com>
2022-02-07 22:56:15 +0100

    rename test

--- tests/map_test.py
+++ tests/map_test.py
@@ -6,7 +6,7 @@ from map_patching import map_patching_utils
 
 
 @pytest.mark.asyncio
-async def test_score_recognition_with_2_players():
+async def test_map_patchin_with_1_map():
     output_file_path, filename = await map_patching_utils.patch_partial_maps(
         "general", ["4c01f257-5a2a-4b62-ba86-d15a3b26cec6"], 400, None)
 
